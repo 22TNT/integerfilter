db2156e38f08b9127b6107c4df03b6deccf4dd7d
22TNT <tnt22@protonmail.com>
2022-10-06 14:23:22 +0700

    Remove repeating code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FormEvent, useState} from 'react';
 import './App.css';
 
+enum Filter {
+  ALL="All",
+  EVEN="Even",
+  ODD="Odd"
+}
+
 function App() {
+  const [nums, setNums] = useState<number[]>([]);
+  const [filter, setFilter] = useState(Filter.ALL);
+  const [inputValue, setInputValue] = useState("");
+  const filters = [Filter.ALL, Filter.EVEN, Filter.ODD];
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputValue) {
+      setNums([...nums, parseInt(inputValue)]);
+    }
+    setInputValue("");
+  }
+
+  function getFiltered() {
+    switch (filter) {
+      case Filter.ODD: return nums.filter(num => num % 2 !== 0);
+      case Filter.EVEN: return nums.filter(num => num % 2 === 0);
+      case Filter.ALL: return nums;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className={"App"}>
+        <form onSubmit={submitHandler}>
+          <input
+              type={"number"}
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button>+</button>
+        </form>
+        <div>
+          {filters.map(item =>
+            <label>
+              <input
+                  type={"radio"}
+                  name={"filter"}
+                  onChange={() => setFilter(item)}
+                  defaultChecked={item === Filter.ALL}
+              />
+              {item}
+            </label>)}
+        </div>
+        <br/>
+        {getFiltered().join(", ")}
+      </div>
+  )
 }
 
 export default App;
